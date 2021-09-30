11bce9bcce426e541abb7233731213a27d1cb624
antolube20 <antolube20@gmail.com>
2021-09-30 15:30:34 -0300

    added routing in dashboardModule, creted component in dashboardModule,dashboard,egress,entry, work in dashboard with grid

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EntryComponent } from './entry/entry.component';
+import { EgressComponent } from './egress/egress.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    EntryComponent,
+    EgressComponent
+
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardRoutingModule,
   ]
 })
 export class DashboardModule { }
