632ae0819eb485b1dbc2867b79a8ebe0553eb82f
antolube20 <antolube20@gmail.com>
2021-10-06 23:12:42 -0300

    Added tables of DB MySQL

---
+++ DB/alkemy_users.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: alkemy
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `createdAt` date DEFAULT NULL,
+  `updatedAt` date DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'anton','1234','anton@gmail.com',NULL,NULL),(2,'gimena','1234','gimena@gmail.com',NULL,NULL),(3,'pablito','1234','pablito@gmail.com','2021-09-17','2021-09-17'),(4,'juancito','$2b$10$IN88aQausCX0M0tHLZRHou.z9.3V1COS7rylvJLVP1vVFDPMk27u.','juancito@gmail.com','2021-09-25','2021-09-25'),(5,'antonio','$2b$10$11aq.YhoERRmQ.FdfDB7zu1at7ApQLHHZJbEd0twUy1TaPqKQx5M2','antonio@gmail.com','2021-09-25','2021-09-25'),(6,'michel','$2b$10$XMqfhKjFe9gO8PWk4uWgDO7lRzASgYl.jy8RTB4HtGAY5fpjRA8VG','michel@gmail.com','2021-09-25','2021-09-25'),(7,'valentin','$2b$10$GJPrLxwhwP8.50U426OlEeF7eSc.tloVAnkQhQ01UM4RPEQp.3.5.','valentin@gmail.com','2021-10-02','2021-10-02'),(8,'mauricio','$2b$10$hEPLMVtUuC1gR2gJ8JNJyOdDKdFMuQwkCwlbm9jhDHATivvs7ui8C','mauricio@gmail.com','2021-10-02','2021-10-02');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-06 23:11:20
